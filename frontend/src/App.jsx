e5a6066e54c84794ae99b3d25f033ac3bfe58eeb
AnupCh37 <anup37@icloud.com>
2026-02-28 11:53:47 +0545

    folder strucutre commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-xl shadow-lg text-center">
+        <h1 className="text-3xl font-bold text-blue-600">
+          Tailwind v4 + React Works! 🎉
+        </h1>
+        <p className="text-gray-500 mt-2">You're all set!</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
