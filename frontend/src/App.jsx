0c3894bc38e1b26ea1d0664e2076f3a23d59cb5f
AnupCh37 <sabinatimalsina2060@gmail.com>
2026-03-03 17:51:22 +0545

    some of the commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,10 @@
 import {  StrictMode } from 'react'
 import './App.css'
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
 import Navbar  from './components/common/Navbar';
 function App() {
  return(
-  <>
-  <BrowserRouter>
-  
-  </BrowserRouter>
-  </>
+  <h1>This is main page</h1>
  )
 }
 export default App;
